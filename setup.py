3d85b9fe2d7fd1a782797b18426739f59a68d608
SheikSadi <sheik.sadi@concured.com>
2022-10-17 23:06:50 +0600

    try this manifest

--- setup.py
+++ setup.py
@@ -7,7 +7,6 @@ setup(
     author_email="biis.saadi@gmail.com",
     include_package_data=True,
     packages=["sam", "weights"],
-    package_dir={"": "src"},
     # dependencies
     install_requires=[
         "keras==1.1.0",
