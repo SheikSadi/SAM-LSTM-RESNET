95bb5c4c61838363918a9d36074ae110978300e2
SheikSadi <sheik.sadi@concured.com>
2022-11-16 22:48:02 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     author_email="biis.saadi@gmail.com",
     include_package_data=True,
     packages=["sam_lstm"],
-    version="1.0.0",
+    version="1.0.1",
     description="Python library to generate saliency maps and smart cropping images",
 )
